5ec9bec9824824fb341e8674d92277480c9ae491
Mochammad Lembar Adjie Bramantya <adjiebramantya.ab@gmail.com>
2021-04-01 22:09:52 +0700

    upt data

--- application/views/barangMasuk.php
+++ application/views/barangMasuk.php
@@ -97,14 +97,14 @@
 															</div>
 															<div class="col-md-6">
 																<div class="form-group form-group-default">
-																	<label>Tanggal</label>
-																	<input id="tanggal" name="tanggal" type="date" class="form-control" placeholder="Masukkan Tanggal">
+																	<label>Jumlah</label>
+																	<input id="jumlah" name="jumlah" type="number" class="form-control" placeholder="Masukkan Stok">
 																</div>
 															</div>
 															<div class="col-md-6">
 																<div class="form-group form-group-default">
-																	<label>Jumlah</label>
-																	<input id="jumlah" name="jumlah" type="number" class="form-control" placeholder="Masukkan Stok">
+																	<label>Tanggal</label>
+																	<input id="tanggal" name="tanggal" type="date" class="form-control" placeholder="Masukkan Tanggal">
 																</div>
 															</div>
 														</div>
