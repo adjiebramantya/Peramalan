804ef8cfb9357b217f8b2336c5481e0c287060d4
Mochammad Lembar Adjie Bramantya <adjiebramantya.ab@gmail.com>
2021-04-06 11:56:25 +0700

    Update peramalan.sql

--- database/peramalan.sql
+++ database/peramalan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2021 at 04:08 AM
+-- Generation Time: Apr 06, 2021 at 06:55 AM
 -- Server version: 10.1.39-MariaDB
 -- PHP Version: 7.3.5
 
