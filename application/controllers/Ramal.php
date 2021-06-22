ebc9d68af226a58054e009b98e116a5affd48d91
Mochammad Lembar Adjie Bramantya <adjiebramantya.ab@gmail.com>
2021-06-23 05:56:13 +0700

    berusaha tampil view

--- application/controllers/Ramal.php
+++ application/controllers/Ramal.php
@@ -22,10 +22,6 @@ class Ramal extends CI_Controller {
 
 	public function ramal_produk($id_produk){
 		$data['produk'] = $this->M_produk->ramal_produk($id_produk)->row();
-		$this->load->view('ramalProduk',$data);
-	}
-
-	public function tesRamal(){
 
 		$awal = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal asc')->row();
 		$akhir = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal desc')->row();
@@ -46,7 +42,7 @@ class Ramal extends CI_Controller {
 								$starter = 1;
 								break;
 						} else {
-								$detail = $this->db->query("SELECT produk.nama_produk as nama_produk, YEAR(tanggal) as tahun, MONTH(tanggal) as Bulan, SUM(jumlah) as jumlah FROM barang_masuk JOIN produk ON barang_masuk.id_produk= produk.id_produk WHERE barang_masuk.id_produk = 26 AND MONTH(tanggal) = '".sprintf("%02d", $b)."' AND YEAR(tanggal) = '".sprintf("%02d", $t)."' GROUP BY MONTH(tanggal), YEAR(tanggal) ORDER BY tanggal asc")->row();
+								$detail = $this->db->query("SELECT produk.nama_produk as nama_produk, YEAR(tanggal) as tahun, MONTH(tanggal) as Bulan, SUM(jumlah) as jumlah FROM barang_masuk JOIN produk ON barang_masuk.id_produk= produk.id_produk WHERE barang_masuk.id_produk = '$id_produk' AND MONTH(tanggal) = '".sprintf("%02d", $b)."' AND YEAR(tanggal) = '".sprintf("%02d", $t)."' GROUP BY MONTH(tanggal), YEAR(tanggal) ORDER BY tanggal asc")->row();
 
 								if ($t == date("Y", strtotime($akhir->tanggal)) && $b > date("m", strtotime($akhir->tanggal))) {
 										break;
@@ -55,11 +51,12 @@ class Ramal extends CI_Controller {
 												//echo sprintf("%02d", $b)."-".$t."==".$detail->nama_produk."|".$detail->jumlah."<br>";
 												$aktual[]= $detail->jumlah;
 												$bulan[] = sprintf("%02d",$b)." - ".$t;
-										} else {
-												//echo sprintf("%02d", $b)."-".$t."==".""."|"."0"."<br>";
-												$aktual[]= 0;
-												$bulan[] = sprintf("%02d",$b)." - ".$t;
 										}
+										// else {
+										// 		//echo sprintf("%02d", $b)."-".$t."==".""."|"."0"."<br>";
+										// 		$aktual[]= 0;
+										// 		$bulan[] = sprintf("%02d",$b)." - ".$t;
+										// }
 								}
 						}
 				}
@@ -172,17 +169,184 @@ class Ramal extends CI_Controller {
 			$hasilMAPE= round($MAPE / $jumlah * 100,3);
 			$hasilKeseluruhan = $hasilMAD + $hasilMSE + $hasilMAPE;
 			$ratarataKesalahan = round($hasilKeseluruhan/3,3);
+
+
+			$data['ramal']['bulan'] = $bulan;
+			$data['ramal']['aktual']= $aktualInt;
+			$data['ramal']['ft']= $ftInt;
+
+		echo "<pre>";
+			print_r($data['ramal']['bulan']);
+		echo "</pre>";
+
+
+		// $this->load->view('ramalProduk',$data);
+	}
+
+	public function tesRamal(){
+
+		$awal = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal asc')->row();
+		$akhir = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal desc')->row();
+
+
+		$starter = 0;
+		$lb = 0;
+
+		for ($t = date("Y", strtotime($awal->tanggal)); $t <= date("Y", strtotime($akhir->tanggal)); $t++) {
+				if ($starter == 0) {
+						$lb = date("m", strtotime($awal->tanggal));
+				} else {
+						$lb = 1;
+				}
+
+				for ($b = $lb; $b <= 13; $b++) {
+						if ($b > 12) {
+								$starter = 1;
+								break;
+						} else {
+								$detail = $this->db->query("SELECT produk.nama_produk as nama_produk, YEAR(tanggal) as tahun, MONTH(tanggal) as Bulan, SUM(jumlah) as jumlah FROM barang_masuk JOIN produk ON barang_masuk.id_produk= produk.id_produk WHERE barang_masuk.id_produk = 12 AND MONTH(tanggal) = '".sprintf("%02d", $b)."' AND YEAR(tanggal) = '".sprintf("%02d", $t)."' GROUP BY MONTH(tanggal), YEAR(tanggal) ORDER BY tanggal asc")->row();
+
+								if ($t == date("Y", strtotime($akhir->tanggal)) && $b > date("m", strtotime($akhir->tanggal))) {
+										break;
+								} else {
+										if ($detail) {
+												//echo sprintf("%02d", $b)."-".$t."==".$detail->nama_produk."|".$detail->jumlah."<br>";
+												$aktual[]= $detail->jumlah;
+												$bulan[] = sprintf("%02d",$b)." - ".$t;
+										}
+										// else {
+										// 		//echo sprintf("%02d", $b)."-".$t."==".""."|"."0"."<br>";
+										// 		$aktual[]= 0;
+										// 		$bulan[] = sprintf("%02d",$b)." - ".$t;
+										// }
+								}
+						}
+				}
+		}
+
+		$alpha = 0.1;
+
+		$aktualInt = array_map('intval',$aktual);
+
+		$nilaiAwal = array_slice( $aktual, 0,1);
+		$nilaiAwalInt = array_map('intval',$nilaiAwal);
+
+		//SMOOTHING 1
+			$s1[]= $nilaiAwalInt[0];
+			$s1Int = array_map('intval',$s1);
+
+			for($i = 1;$i < count($aktualInt)+1;$i++)
+				{
+					$s1Int[$i]= round($alpha * $aktualInt[$i-1]+(1-$alpha) * $s1Int[$i-1],2);
+				}
+
+		//SMOOTHING 2
+			$s2[]= $nilaiAwalInt[0];
+			$s2Int = array_map('intval',$s2);
+
+			for($i = 1;$i < count($s1Int);$i++)
+				{
+					$s2Int[$i]= round($alpha * $s1Int[$i]+(1-$alpha) * $s2Int[$i-1],2);
+				}
+
+		//SMOOTHING 3
+			$s3[]= $nilaiAwalInt[0];
+			$s3Int = array_map('intval',$s2);
+
+			for($i = 1;$i < count($s2Int);$i++)
+				{
+					$s3Int[$i]= round($alpha * $s2Int[$i]+(1-$alpha) * $s3Int[$i-1],2);
+				}
+
+				//AT
+					$at = array();
+					$atInt = array_map('intval',$at);
+
+					for($i = 1;$i < count($s1Int);$i++)
+						{
+							$atInt[$i]= round(3 * $s1Int[$i] - 3 * $s2Int[$i] + $s3Int[$i],2);
+						}
+
+				//BT
+				$bt = array();
+				$btInt = array_map('intval',$bt);
+
+				for($i = 1;$i < count($s1Int);$i++)
+					{
+						$btInt[$i]= round($alpha/2*pow(1-$alpha,2)*((6-5*$alpha)*$s1Int[$i]-(10-8*$alpha)*$s2Int[$i]+(4-3*$alpha)*$s3Int[$i]),2);
+					}
+
+				//CT
+					$ct = array();
+					$ctInt = array_map('intval',$ct);
+
+					for($i = 1;$i < count($s1Int);$i++)
+						{
+							$ctInt[$i]= round(pow($alpha,2)/pow(1-$alpha,2)*($s1Int[$i]-2*$s2Int[$i]+$s3Int[$i]),2);
+						}
+
+				//FT+M
+					$ft = array();
+					$ftInt = array_map('intval',$ft);
+
+					for($i = 1;$i < count($atInt)+1;$i++)
+						{
+							$ftInt[$i]= round($atInt[$i]+$btInt[$i]*1+1/2*$ctInt[$i]*pow(1,2),0);
+						}
+
+				//at-ft
+					$selisih = array();
+					$selisihInt = array_map('intval',$selisih);
+
+					for($i = 1;$i < count($aktualInt);$i++)
+						{
+							$selisihInt[$i]= $aktualInt[$i]-$ftInt[$i];
+						}
+
+				//(at-ft)2
+					$selisihPangkat = array();
+					$selisihPangkatInt = array_map('intval',$selisihPangkat);
+
+					for($i = 1;$i < count($selisihInt)+1;$i++)
+						{
+							$selisihPangkatInt[$i]= pow($selisihInt[$i],2);
+						}
+
+			//abs((at-ft)/at)*100
+				$selisihSeratus = array();
+				$selisihSeratusInt = array_map('intval',$selisihSeratus);
+
+				for($i = 1;$i < count($selisihInt)+1;$i++)
+					{
+						$selisihSeratusInt[$i]= abs($selisihInt[$i]/$aktualInt[$i]);
+					}
+
+			$jumlah= count(array_slice($aktualInt,1));
+			$MAD = array_sum(array_map("abs",$selisihInt));
+			$MSE = array_sum($selisihPangkatInt);
+			$MAPE = round(array_sum($selisihSeratusInt),3);
+
+			$hasilMAD = round($MAD / $jumlah,3);
+			$hasilMSE = round($MSE / ($jumlah-1),3);
+			$hasilMAPE= round($MAPE / $jumlah * 100,3);
+			$hasilKeseluruhan = $hasilMAD + $hasilMSE + $hasilMAPE;
+			$ratarataKesalahan = round($hasilKeseluruhan/3,3);
+
+
+			$data['bulan'] = $bulan;
+			$data['aktual']= $aktualInt;
+			$data['ft']= $ftInt;
 		// echo "<pre>";
 		// 	print_r($nilaiAwalInt);
 		// echo "</pre>";
 		echo "<br>";
 		echo "<p>AKtual</p>";
 		echo "<pre>";
-			print_r($aktualInt);
+			print_r($data['aktual']);
 		echo "</pre>";
 		echo "<p>S1</p>";
 		echo "<pre>";
-			print_r($s1Int);
+			print_r($data['s1']);
 		echo "</pre>";
 		echo "<p>S2</p>";
 		echo "<pre>";
--- application/views/ramalProduk.php
+++ application/views/ramalProduk.php
@@ -47,230 +47,7 @@
 				</div>
 
 				<!-- RUMUS TRIPLE EXPONENTIAL SMOOTHING -->
-				<?php
 
-				$awal = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal asc')->row();
-				$akhir = $this->db->query('SELECT tanggal FROM barang_masuk order by tanggal desc')->row();
-
-
-				$starter = 0;
-				$lb = 0;
-
-				for ($t = date("Y", strtotime($awal->tanggal)); $t <= date("Y", strtotime($akhir->tanggal)); $t++) {
-						if ($starter == 0) {
-								$lb = date("m", strtotime($awal->tanggal));
-						} else {
-								$lb = 1;
-						}
-
-						for ($b = $lb; $b <= 13; $b++) {
-								if ($b > 12) {
-										$starter = 1;
-										break;
-								} else {
-									if (isset($produk)) {
-										$id_produk = $produk->id_produk;
-									}
-
-										$detail = $this->db->query("SELECT produk.nama_produk as nama_produk, YEAR(tanggal) as tahun, MONTH(tanggal) as Bulan, SUM(jumlah) as jumlah FROM barang_masuk JOIN produk ON barang_masuk.id_produk= produk.id_produk WHERE barang_masuk.id_produk = '$id_produk' AND MONTH(tanggal) = '".sprintf("%02d", $b)."' AND YEAR(tanggal) = '".sprintf("%02d", $t)."' GROUP BY MONTH(tanggal), YEAR(tanggal) ORDER BY tanggal asc")->row();
-
-										if ($t == date("Y", strtotime($akhir->tanggal)) && $b > date("m", strtotime($akhir->tanggal))) {
-												break;
-										} else {
-												if ($detail) {
-														//echo sprintf("%02d", $b)."-".$t."==".$detail->nama_produk."|".$detail->jumlah."<br>";
-														$aktual[]= $detail->jumlah;
-														$bulan[] = sprintf("%02d",$b)." - ".$t;
-												} else {
-														//echo sprintf("%02d", $b)."-".$t."==".""."|"."0"."<br>";
-														$aktual[]= 0;
-														$bulan[] = sprintf("%02d",$b)." - ".$t;
-												}
-										}
-								}
-						}
-				}
-
-				$alpha = 0.1;
-
-				$aktualInt = array_map('intval',$aktual);
-
-				$nilaiAwal = array_slice( $aktual, 0,1);
-				$nilaiAwalInt = array_map('intval',$nilaiAwal);
-
-				//SMOOTHING 1
-					$s1[]= $nilaiAwalInt[0];
-					$s1Int = array_map('intval',$s1);
-
-					for($i = 1;$i < count($aktualInt)+1;$i++)
-						{
-							$s1Int[$i]= round($alpha * $aktualInt[$i-1]+(1-$alpha) * $s1Int[$i-1],2);
-						}
-
-				//SMOOTHING 2
-					$s2[]= $nilaiAwalInt[0];
-					$s2Int = array_map('intval',$s2);
-
-					for($i = 1;$i < count($s1Int);$i++)
-						{
-							$s2Int[$i]= round($alpha * $s1Int[$i]+(1-$alpha) * $s2Int[$i-1],2);
-						}
-
-				//SMOOTHING 3
-					$s3[]= $nilaiAwalInt[0];
-					$s3Int = array_map('intval',$s2);
-
-					for($i = 1;$i < count($s2Int);$i++)
-						{
-							$s3Int[$i]= round($alpha * $s2Int[$i]+(1-$alpha) * $s3Int[$i-1],2);
-						}
-
-						//AT
-							$at = array();
-							$atInt = array_map('intval',$at);
-
-							for($i = 1;$i < count($s1Int);$i++)
-								{
-									$atInt[$i]= round(3 * $s1Int[$i] - 3 * $s2Int[$i] + $s3Int[$i],2);
-								}
-
-						//BT
-						$bt = array();
-						$btInt = array_map('intval',$bt);
-
-						for($i = 1;$i < count($s1Int);$i++)
-							{
-								$btInt[$i]= round($alpha/2*pow(1-$alpha,2)*((6-5*$alpha)*$s1Int[$i]-(10-8*$alpha)*$s2Int[$i]+(4-3*$alpha)*$s3Int[$i]),2);
-							}
-
-						//CT
-							$ct = array();
-							$ctInt = array_map('intval',$ct);
-
-							for($i = 1;$i < count($s1Int);$i++)
-								{
-									$ctInt[$i]= round(pow($alpha,2)/pow(1-$alpha,2)*($s1Int[$i]-2*$s2Int[$i]+$s3Int[$i]),2);
-								}
-
-						//FT+M
-							$ft = array();
-							$ftInt = array_map('intval',$ft);
-
-							for($i = 1;$i < count($atInt)+1;$i++)
-								{
-									$ftInt[$i]= round($atInt[$i]+$btInt[$i]*1+1/2*$ctInt[$i]*pow(1,2),0);
-								}
-
-						//at-ft
-							$selisih = array();
-							$selisihInt = array_map('intval',$selisih);
-
-							for($i = 1;$i < count($aktualInt);$i++)
-								{
-									$selisihInt[$i]= $aktualInt[$i]-$ftInt[$i];
-								}
-
-						//(at-ft)2
-							$selisihPangkat = array();
-							$selisihPangkatInt = array_map('intval',$selisihPangkat);
-
-							for($i = 1;$i < count($selisihInt)+1;$i++)
-								{
-									$selisihPangkatInt[$i]= pow($selisihInt[$i],2);
-								}
-
-					//abs((at-ft)/at)*100
-						$selisihSeratus = array();
-						$selisihSeratusInt = array_map('intval',$selisihSeratus);
-
-						for($i = 1;$i < count($selisihInt)+1;$i++)
-							{
-								$selisihSeratusInt[$i]= abs($selisihInt[$i]/$aktualInt[$i]);
-							}
-
-					$jumlah= count(array_slice($aktualInt,1));
-					$MAD = array_sum(array_map("abs",$selisihInt));
-					$MSE = array_sum($selisihPangkatInt);
-					$MAPE = round(array_sum($selisihSeratusInt),3);
-
-					$hasilMAD = round($MAD / $jumlah,3);
-					$hasilMSE = round($MSE / ($jumlah-1),3);
-					$hasilMAPE= round($MAPE / $jumlah * 100,3);
-					$hasilKeseluruhan = $hasilMAD + $hasilMSE + $hasilMAPE;
-					$ratarataKesalahan = round($hasilKeseluruhan/3,3);
-				// echo "<pre>";
-				// 	print_r($nilaiAwalInt);
-				// echo "</pre>";
-				echo "<br>";
-				echo "<p>AKtual</p>";
-				echo "<pre>";
-					print_r($bulan);
-				echo "</pre>";
-				echo "<p>S1</p>";
-				echo "<pre>";
-					print_r($s1Int);
-				echo "</pre>";
-				echo "<p>S2</p>";
-				echo "<pre>";
-					print_r($s2Int);
-				echo "</pre>";
-				echo "<p>S3</p>";
-				echo "<pre>";
-					print_r($s3Int);
-				echo "</pre>";
-				echo "<p>at</p>";
-				echo "<pre>";
-					print_r($atInt);
-				echo "</pre>";
-				echo "<p>bt</p>";
-				echo "<pre>";
-					print_r($btInt);
-				echo "</pre>";
-				echo "<p>ct</p>";
-				echo "<pre>";
-					print_r($ctInt);
-				echo "</pre>";
-				echo "<p>Ft+M</p>";
-				echo "<pre>";
-					print_r($ftInt);
-				echo "</pre>";
-				echo "<p>Selisih</p>";
-				echo "<pre>";
-					print_r($selisihInt);
-				echo "</pre>";
-				echo "<p>Selisih Absolute</p>";
-				echo "<pre>";
-					print_r(array_map("abs",$selisihInt));
-				echo "</pre>";
-				echo "<p>Selisih Pangkat</p>";
-				echo "<pre>";
-					print_r($selisihPangkatInt);
-				echo "</pre>";
-				echo "<p>Selisih Seratus</p>";
-				echo "<pre>";
-					print_r($selisihSeratusInt);
-				echo "</pre>";
-				echo "<p>MAD</p>";
-				echo "<pre>";
-					print_r($hasilMAD);
-				echo "</pre>";
-				echo "<p>MSE</p>";
-				echo "<pre>";
-					print_r($hasilMSE);
-				echo "</pre>";
-				echo "<p>MAPE</p>";
-				echo "<pre>";
-					print_r($hasilMAPE);
-				echo "</pre>";
-				echo "<p>Hasil Keseluruhan</p>";
-				echo "<pre>";
-					print_r($hasilKeseluruhan);
-				echo "</pre>";
-				echo "<p>Rata- Rata Kesalahan</p>";
-				echo "<pre>";
-					print_r($ratarataKesalahan);
-				echo "</pre>";
-				 ?>
 				<!-- END RUMUS TRIPLE EXPONENTIAL SMOOTHING -->
 
 				<div class="page-inner mt--5">
@@ -317,24 +94,29 @@
 										<table id="add-row" class="display table table-striped table-hover" >
 											<thead>
 												<tr>
-													<th>Name</th>
-													<th>Position</th>
+													<th>Bulan</th>
+													<th>Jumlah Aktual</th>
 													<th>Office</th>
 													<th style="width: 10%">Action</th>
 												</tr>
 											</thead>
 											<tfoot>
 												<tr>
-													<th>Name</th>
-													<th>Position</th>
+													<th>Bulan</th>
+													<th>Jumlah Aktual</th>
 													<th>Office</th>
 													<th>Action</th>
 												</tr>
 											</tfoot>
 											<tbody>
+											<?php foreach ($ramal as $data): ?>
 												<tr>
-													<td>Tiger Nixon</td>
-													<td>System Architect</td>
+													<td>
+														<?php echo $data['bulan'] ?>
+													</td>
+													<td>
+														<?php echo $data['aktual'] ?>
+													</td>
 													<td>Edinburgh</td>
 													<td>
 														<div class="form-button-action">
@@ -347,6 +129,7 @@
 														</div>
 													</td>
 												</tr>
+											<?php endforeach; ?>
 											</tbody>
 										</table>
 									</div>
